975e075de53e60c8bb44a136c780f1cadc607bb3
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2012-08-13 06:49:19 +0000

    Import of TOKUHIROM/Data-Difflet-0.02 from CPAN.
    gitpan-cpan-distribution: Data-Difflet
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TOKUHIROM/Data-Difflet-0.02.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- eg/test.pl
+++ eg/test.pl
@@ -7,23 +7,11 @@ use autodie;
 
 use Test::More;
 use Test::Deep;
-use Data::Difflet;
+use Test::Difflet qw/is_deeply/;
 
-sub equivalent {
-    my ($got, $expected, $msg) = @_;
-    my $builder = Test::More->builder;
+is_deeply( { "foo" => [ 1, 2, 3 ] }, { "foo" => [ 4, 2, 3 ] } );
+is_deeply( { "foo" => [ 1, 2, 3 ] }, { "foo" => [ 1, 2, 3 ] } );
 
-    if (eq_deeply($got, $expected)) {
-        local $Test::Builder::Level = $Test::Builder::Level + 1;
-        $builder->ok(1, $msg);
-    } else {
-        local $Test::Builder::Level = $Test::Builder::Level + 1;
-        my $difflet = Data::Difflet->new();
-        $builder->ok(0, $msg);
-        $builder->diag($difflet->compare($got, $expected));
-    }
-}
-
-equivalent( { "foo" => [ 1, 2, 3 ] }, { "foo" => [ 4, 2, 3 ] } );
+difflet_is_deeply( { "foo" => [ 1, 2, 3 ] }, { "foo" => [ 4, 2, 3 ] } );
 
 done_testing;
